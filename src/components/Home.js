6078abb4ef514afcaca48a5ee843e3f7a600d6a1
James Kamau <jamesnk5773@gmail.com>
2025-07-09 17:20:04 +0300

    compiler

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,17 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const stylePage= {
+    color:"firebrick"
+  }
+  
+  return ( <div id="home">
+    <h1 style={stylePage}>
+      {name}is a Web Developer from {city}
+    </h1>
+    
+  </div>
+  )
 }
 
 export default Home;
